e76f64270b1e7711845b3c13098f97230b7796e0
Jesse Oldershaw <jesse.oldershaw@rpharms.com>
2020-10-03 14:38:59 +0100

    add next game states

--- app/models/GameState.scala
+++ app/models/GameState.scala
@@ -2,9 +2,15 @@ package models
 
 sealed trait GameState {
   def playerId: Int
+  def nextGameState: GameState = this match {
+    case AddPlayer(0) => AddPlayer(1)
+    case AddPlayer(1) => NextPlayer(0)
+    case NextPlayer(0) => NextPlayer(1)
+    case NextPlayer(1) => NextPlayer(0)
+    case PlayerWon(_) => AddPlayer(0)
+  }
 }
 final case class AddPlayer(playerId: Int) extends GameState
 final case class NextPlayer(playerId: Int) extends GameState
 final case class PlayerWon(playerId: Int) extends GameState
-//final case object NewGame extends GameState
 
