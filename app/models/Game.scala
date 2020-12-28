8ed5d4b0f4af74dbd45d63105d176bbb37df6242
Jesse <folde01@users.noreply.github.com>
2020-12-28 12:01:42 +0100

    deduplicate nextPlayer and guess

--- app/models/Game.scala
+++ app/models/Game.scala
@@ -5,10 +5,39 @@ import scala.collection.mutable.ListBuffer
 
 case class Game() {
 
+  private var gameState: GameState = AddPlayer(0)
+
+  def getGameState: GameState = gameState
+
   def wordIsInvalid(word: Word): Boolean = word.isInvalid
 
-  def guess(guesserId: Int, word: Word, guesseeId: Int): Option[Int] = {
-    players(guesseeId).secretWord.lettersInCommon(word)
+//  def guess(guesserId: Int, word: Word, guesseeId: Int): Option[Int] = {
+//    players(guesseeId).secretWord.lettersInCommon(word)
+//  }
+
+  def guess(guesserId: Int, word: Word, guesseeId: Int): Option[Answer] = {
+      if (wordIsInvalid(word))
+        None
+      else if (gameState == NextPlayer(guesserId)) {
+        val numberOfMatchingLetters: Option[Int] = players(guesseeId).secretWord.lettersInCommon(word)
+
+        numberOfMatchingLetters match {
+          case None => None
+          case Some(n: Int) =>
+            if (n == Word.WORD_LENGTH)
+              gameState = PlayerWon(guesserId)
+            else {
+              nextPlayer
+            }
+
+            val answer: Answer = Answer(guesserId, word, n, gameState)
+            players(guesserId).addAnswer(answer)
+            Some(answer)
+        }
+
+      }
+      else
+        None
   }
 
   private var players: ListBuffer[Player] = ListBuffer.empty
@@ -57,61 +86,6 @@ case class Game() {
     }
   }
 
-  private var gameState: GameState = AddPlayer(0)
-
-  def getGameState: GameState = gameState
-
-}
-
-object Game {
-
-//  private var game: Option[Game] = None
-
-//  private var gameState: GameState = AddPlayer(0)
-
-//  def getGameState: GameState = gameState
-
-//  def newGame: Option[Game] = {
-//    gameState = AddPlayer(0)
-//    game = Some(Game())
-//    game
-//  }
-
-//  def playerName(id: Int): String = game match {
-//    case None => "UNKNOWN_PLAYER"
-//    case Some(Game()) => game.get.playerName(id)
-//  }
-
-//  def playerAnswers(id: Int): Seq[Answer] = getGame.playerAnswers(id)
-
-//  private def getGame: Game = game match {
-//    case None => newGame.get
-//    case Some(_) => game.get
-//  }
-
-//  def addPlayer(playerId: Int, name: String, secretWord: Word): GameState = {
-//
-//    val nextGameState: GameState = playerId match {
-//      case 0 => AddPlayer(1)
-//      case 1 => NextPlayer(0)
-//      case _ => gameState
-//    }
-//
-//    gameState match {
-//      case AddPlayer(n) =>
-//        if (playerId.equals(n)) {
-//          getGame.addPlayer(Player(n, name, secretWord: Word)) match {
-//            case None =>
-//            case Some(playerId: Int) =>
-//              if (playerId.equals(n))
-//                gameState = nextGameState
-//          }
-//        }
-//        gameState
-//
-//      case _ => gameState
-//    }
-//  }
 
   def nextPlayer: Int = {
     gameState = gameState match {
@@ -122,32 +96,93 @@ object Game {
     gameState.playerId
   }
 
-  def guess(guesserId: Int, word: Word, guesseeId: Int): Option[Answer] = game match {
-    case None =>
-      newGame
-      None
+}
 
-    case Some(game) =>
-      if (game.wordIsInvalid(word)) None
+object Game {
 
-      else if (gameState == NextPlayer(guesserId)) {
-        val numberOfMatchingLetters: Option[Int] = getGame.guess(guesserId, word, guesseeId)
+  //  private var game: Option[Game] = None
 
-        numberOfMatchingLetters match {
-          case None => None
-          case Some(n: Int) =>
-            if (n == Word.WORD_LENGTH)
-              gameState = PlayerWon(guesserId)
-            else {
-              nextPlayer
-            }
+  //  private var gameState: GameState = AddPlayer(0)
 
-            val answer: Answer = Answer(guesserId, word, n, gameState)
-            getGame.players(guesserId).addAnswer(answer)
-            Some(answer)
-        }
+  //  def getGameState: GameState = gameState
 
-      } else None
-  }
+  //  def newGame: Option[Game] = {
+  //    gameState = AddPlayer(0)
+  //    game = Some(Game())
+  //    game
+  //  }
+
+  //  def playerName(id: Int): String = game match {
+  //    case None => "UNKNOWN_PLAYER"
+  //    case Some(Game()) => game.get.playerName(id)
+  //  }
+
+  //  def playerAnswers(id: Int): Seq[Answer] = getGame.playerAnswers(id)
+
+  //  private def getGame: Game = game match {
+  //    case None => newGame.get
+  //    case Some(_) => game.get
+  //  }
+
+  //  def addPlayer(playerId: Int, name: String, secretWord: Word): GameState = {
+  //
+  //    val nextGameState: GameState = playerId match {
+  //      case 0 => AddPlayer(1)
+  //      case 1 => NextPlayer(0)
+  //      case _ => gameState
+  //    }
+  //
+  //    gameState match {
+  //      case AddPlayer(n) =>
+  //        if (playerId.equals(n)) {
+  //          getGame.addPlayer(Player(n, name, secretWord: Word)) match {
+  //            case None =>
+  //            case Some(playerId: Int) =>
+  //              if (playerId.equals(n))
+  //                gameState = nextGameState
+  //          }
+  //        }
+  //        gameState
+  //
+  //      case _ => gameState
+  //    }
+  //  }
+
+  //  def nextPlayer: Int = {
+  //    gameState = gameState match {
+  //      case NextPlayer(0) => NextPlayer(1)
+  //      case NextPlayer(1) => NextPlayer(0)
+  //      case _ => gameState
+  //    }
+  //    gameState.playerId
+  //  }
+
+  //  def guess(guesserId: Int, word: Word, guesseeId: Int): Option[Answer] = game match {
+  //    case None =>
+  //      newGame
+  //      None
+  //
+  //    case Some(game) =>
+  //      if (game.wordIsInvalid(word)) None
+  //
+  //      else if (gameState == NextPlayer(guesserId)) {
+  //        val numberOfMatchingLetters: Option[Int] = getGame.guess(guesserId, word, guesseeId)
+  //
+  //        numberOfMatchingLetters match {
+  //          case None => None
+  //          case Some(n: Int) =>
+  //            if (n == Word.WORD_LENGTH)
+  //              gameState = PlayerWon(guesserId)
+  //            else {
+  //              nextPlayer
+  //            }
+  //
+  //            val answer: Answer = Answer(guesserId, word, n, gameState)
+  //            getGame.players(guesserId).addAnswer(answer)
+  //            Some(answer)
+  //        }
+  //
+  //      } else None
+  //  }
 
 }
